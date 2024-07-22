1f868e60e7213532fe1bd41051bf656fa447a443
EnockYator <ekyator02@gmail.com>
2024-07-22 08:45:45 +0300

    added tailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,53 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default <Partial<Config>>{
   theme: {
     extend: {
-
-        // enabling default aspect ratio utilities: (disabled by @tailwindcss/aspect-ratio plugin)
-        // [aspect-auto, aspect-square, aspect-video]
+      // enabling default aspect ratio utilities: (disabled by @tailwindcss/aspect-ratio plugin)
+      // [aspect-auto, aspect-square, aspect-video]
+      
       aspectRatio: {
-        auto: 'auto',
-        square: '1 / 1',
-        video: '16 / 9'
+        auto: "auto",
+        square: "1 / 1",
+        video: "16 / 9",
+      },
+      // adding additional colors
+      colors: {
+        green: {
+          50: '#EFFDF5',
+          100: '#D9FBE8',
+          200: '#B3F5D1',
+          300: '#75EDAE',
+          400: '#00DC82',
+          500: '#00C16A',
+          600: '#00A155',
+          700: '#007F45',
+          800: '#016538',
+          900: '#0A5331',
+          950: '#052e16'
+        }
       }
-    }
-  }
-}
+    },
+  
+    screens: {
+      'xsm': '400px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+  },
+    
+  },
+
+  content: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "docs/content/**/*.md"
+  ],
+  plugins: [],
+};
