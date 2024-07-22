c48c372a7f0a4dbef8dfb662c6449520e5e1100b
EnockYator <ekyator02@gmail.com>
2024-07-22 08:46:22 +0300

    added nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,24 @@
+import { _backgroundColor } from "#tailwind-config/theme";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
+  modules: [
+    "@nuxt/ui"
+
+  ],
+  colorMode: {
+    preference: 'light'
+  },
+  loadingIndicator: {
+    name: 'circle',
+    color: '#3B8070',
+    background: 'white'
+  },
+
+  ui: {
+    safelistColors: ['blue' /*safe custom colors*/]
+  }
+
 })
