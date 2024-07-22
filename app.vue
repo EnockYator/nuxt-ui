33df239611ae3e22505d7e1db174f00875c9ec9f
EnockYator <ekyator02@gmail.com>
2024-07-22 08:48:00 +0300

    defined app entrypoint

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+
+<script setup lang="ts">
+import { _scale, _width } from "#tailwind-config/theme";
+import { definePageMeta } from "./node_modules/nuxt/dist/pages/runtime/composables";
+
+definePageMeta({
+  middleware: ['auth'],
+  transition: {
+    name: 'fade',
+    mode: 'out-in'
+  },
+});
+</script>
+
