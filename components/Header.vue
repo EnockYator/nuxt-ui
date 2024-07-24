1ae6bca322a6e223e630d7bc9169fbd820d9305c
EnockYator <ekyator02@gmail.com>
2024-07-24 15:20:24 +0300

    improved responsiveness on smaller screens

--- components/Header.vue
+++ components/Header.vue
@@ -1,35 +1,60 @@
 <template>
   <header class="w-screen">
-    <div class="grid lg:flex justify-between items-center px-4 py-3 bg-blue-700 dark:bg-slate-950 text-white">
+    <div class="grid lg:flex justify-between items-center px-4 py-3 bg-blue-800 dark:bg-slate-950 text-white">
 
       <div class="flex justify-between w-screen">
         <!-- Social media icons-->
         <div class="flex justify-between">
           <div class="text-orange-400 font-serif text-xl lg:text-3xl font-extrabold italic underline">attachME</div>
-          <div class="flex md:space-x-3 lg:flex">
-            <UButton icon="i-heroicons-magnifying-glass text-gray-100 hover:bg-blue-900" class="hover:bg-white"
+          <div class="flex md:space-x-3 mx-2 lg:flex">
+            <!--facebook icon-->
+            <UButton 
+              icon="i-heroicons-magnifying-glass"
+              color="white"
+              variant="ghost"
+              class="hover:bg-blue-500 dark:hover:bg-blue-700 hover:color-blue text-white h-8 my-auto"
               to="#" />
-            <UButton icon="i-heroicons-magnifying-glass text-gray-100 hover:bg-blue-900" class="hover:bg-white"
+            <!--Instagram icon-->
+            <UButton 
+              icon="i-heroicons-magnifying-glass"
+              color="white"
+              variant="ghost"
+              class="hover:bg-blue-500 dark:hover:bg-blue-700 hover:color-blue text-white h-8 my-auto"
               to="#" />
-            <UButton icon="i-heroicons-magnifying-glass text-gray-100 hover:bg-blue-900" class="hover:bg-white"
-              to="#" />
+            <!--twitter icon-->
+            <UButton 
+              icon="i-heroicons-magnifying-glass"
+              color="white"
+              variant="ghost"
+              class="hover:bg-blue-500 dark:hover:bg-blue-700 hover:color-blue text-white h-8 my-auto"
+              to="#" />            
           </div>
         </div>
 
         <!-- Theme toggle button -->
         <div class="">
           <ClientOnly>
-            <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'
-              " color="none" @click="isDark = !isDark" class=" md:mr-0 hover:bg-none" />
-            <template #fallback>
-              <div class="w-8 h-8"></div>
-            </template>
+            <UButton 
+              :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+              color="white"
+              variant="ghost"
+              class="hover:bg-blue-600 dark:hover:bg-slate-600 hover:color-blue text-white h-8 my-auto rounded-full"
+              @click="isDark = !isDark" />
+              <template #fallback>
+                <div class="w-8 h-8"></div>
+              </template>
           </ClientOnly>
         </div>
 
         <!--Menu icon for displaying nav menu-->
         <div class="md:hidden mr-8">
-          <UButton icon="i-heroicons-bars-3 bg-white" class="bg-none hover:bg-slate-400" @click="toggleMenu" />
+          <UButton
+            icon="i-heroicons-bars-3"
+            color="white"
+            variant="ghost"
+            class="hover:bg-blue-600 dark:hover:bg-slate-600 hover:color-blue text-white h-8 my-auto rounded-md"
+            @click="toggleMenu" />
+          
         </div>
 
         <!--Navigation links-->
@@ -51,23 +76,31 @@
       <div class="flex justify-center mx-auto my-2 lg:mr-6 lg:ml-6">
         <input type="text" name="search" placeholder="Search Here"
           class="px-3 focus:outline-none rounded-2xl mr-1 dark:bg-slate-500  text-black dark:text-slate-50 text-center w-48 h-8" />
-        <UButton icon="i-heroicons-magnifying-glass text-blue-800 dark:text-slate-50"
-          class="bg-slate-400 dark:bg-slate-600 h-8" />
+        <UButton
+          icon="i-heroicons-magnifying-glass"
+          color="white"
+          variant="ghost"
+          class="hover:bg-blue-600 dark:hover:bg-slate-600 hover:color-blue text-white h-8 my-auto rounded-md"
+          />
       </div>
     </div>
 
     <!--Navigation links on mobiles-->
     <div :class="{ 'hidden': !isMenuOpen, 'block': isMenuOpen }"
-      class="fixed inset-x-1/4 inset-y-0 z-50 w-3/4  bg-blue-600 dark:bg-slate-900 md:hidden">
+      class="fixed h-screen inset-x-1/4 inset-y-0 z-50 w-3/4 sm:inset-x-1/2 sm:w-1/2 bg-blue-600 dark:bg-slate-900 md:hidden">
 
       <!--Menu icon for hiding nav menu-->
       <div class="ml-6 mt-4">
-        <UButton icon="i-heroicons-x-mark bg-white" class="bg-none hover:bg-slate-400" @click="toggleMenu" />
+        <UButton
+          icon="i-heroicons-x-mark bg-white"
+          variant="outline"
+          class="hover:bg-slate-600 dark:hover:bg-slate-600 hover:color-blue text-white h-8 my-auto rounded-md"
+          @click="toggleMenu" />
       </div>
 
       <nav class="grid space-y-6 py-12 text-base w-full justify-evenly text-white font-bold">
-        <nuxt-link to="#" class="hover:text-orange-300">About</nuxt-link>
         <nuxt-link to="/" class="hover:text-orange-300">Home</nuxt-link>
+        <nuxt-link to="#" class="hover:text-orange-300">About</nuxt-link>
         <nuxt-link to="#" class="hover:text-orange-300">How to Apply</nuxt-link>
         <nuxt-link to="#" class="hover:text-orange-300">Contact us</nuxt-link>
         <nuxt-link to="#" class="hover:text-orange-300">FAQs</nuxt-link>
@@ -86,6 +119,7 @@
 </template>
 
 <script setup lang="ts">
+import type { _size } from '#tailwind-config/theme';
 import { _white } from '#tailwind-config/theme/accentColor';
 
 //Theme toggle button
